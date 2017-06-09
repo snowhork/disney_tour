074edb248a085301fb7b3221316fa892f8a4f56f
kyoh <stayheresilence@gmail.com>
2017-06-09 10:19:58 +0900

    fix controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,14 +9,16 @@ class HomeController < ApplicationController
   def calc
     # pythonコール
     date_params =  params[:departed][:date].clone
+    date_params = '2017/04/29'
     departed_time_params = params[:departed_time].clone
-    if date_params.sub(%r{(\d{4})/(\d{2})/(\d{2})}, '\1\2\3') &&
-        departed_time_params.sub(%r{(\d{2}):\d{2}}, '\1')
-      system('python', "#{Rails.root.to_s}/lib/others/python/predict_wait_time.py", '20170401', '8', '1')
-      #logger.info("execute python predict_wait_time.py #{date_params} #{departed_time_params} #{weather_state.to_s}")
+    finished_time_params = params[:finished_time].clone
+
+    unless date_params.sub!(%r{(\d{4})/(\d{2})/(\d{2})}, '\1\2\3') &&
+        departed_time_params.match(%r{(\d{2}):(\d{2})}) &&
+        finished_time_params.match(%r{(\d{2}):(\d{2})})
+      return
     end
 
-    # input_json を生成
     user_input_json_path = "#{Rails.root.to_s}/lib/others/cpp/input/user_input.json"
     user_input =
         {
@@ -24,7 +26,8 @@ class HomeController < ApplicationController
                 list:  params[:attraction_ids].map { |k, v| { ID: k.to_i, hope: v.to_i} },
                 start: params[:departed_time],
                 end:   params[:finished_time],
-                position: 0
+                position: 0,
+                date: date_params
             }
         }
 
@@ -33,50 +36,29 @@ class HomeController < ApplicationController
     end
 
     # Cコール
-    logger.info('route_search.out')
-    system("#{Rails.root.to_s}/lib/others/cpp/a.out", "#{Rails.root.to_s}/lib/others/cpp/")
+    system("#{Rails.root.to_s}/lib/others/cpp/route_algorithm.out", "#{Rails.root.to_s}/lib/others/cpp/", date_params)
+
+    depareted_minute = to_minute(departed_time_params)
+    finished_minute = to_minute(finished_time_params)
+
+    logger.info("date_params: #{date_params}, departed_minute: #{depareted_minute}, finished_minute: #{finished_minute}")
+    system("#{Rails.root.to_s}/lib/others/cpp/kikaigakushu_once.out", "#{Rails.root.to_s}/lib/others/cpp/", date_params, depareted_minute, finished_minute)
 
     # 結果読み込み
     result = {}
     File.open("#{Rails.root.to_s}/lib/others/cpp/output/route_output.json") do |file|
       result = JSON.load(file)
     end
-    logger.info(result)
 
-
-
-
-    @candidates = result['candidates'].map do |candidate|
-      { start: {
-          id:     candidate['start']['place'],
-          area_id: Attraction.find_by(algorithm_id: candidate['start']['place']).area_id,
-          name: '出発' || Attraction.find_by(algorithm_id: candidate['start']['place']).name,
-          time:  candidate['start']['time']
-        },
-        attractions: candidate['attraction'].map { |attraction|
-          {
-            id:    attraction['ID'],
-            name:   attraction_name(attraction),
-            area_id: Attraction.find_by(algorithm_id: attraction['ID']).area_id,
-            start:      attraction['start'],
-            move:       attraction['move'],
-            arrive:     attraction['arrive'],
-            wait:       attraction['wait'],
-            ride:       attraction['ride'],
-            duration:   attraction['duration'],
-            end:        attraction['end']
-          }
-        },
-        discription: candidate['discription']
-      }
+    result2 = {}
+    File.open("#{Rails.root.to_s}/lib/others/cpp/output/route_output2.json") do |file|
+      result2 = JSON.load(file)
     end
-  end
-  class StartInfo
-    attr_accessor :position_id, :attraction_name, :start_datetime
-  end
 
-  class AttractionInfo
-    attr_accessor :algorithm_id, :attraction_name, :move_time, :arrive_time, :wait_time, :ride_time, :duration_time, :end_time
+
+    @candidates = result['candidates'].map { |c| parse_candidate(c)}
+
+    @candidates << parse_candidate(result2['candidates'][0])
   end
 
   private
@@ -98,7 +80,36 @@ class HomeController < ApplicationController
 
   def attraction_name(attraction)
     name = Attraction.find_by(algorithm_id: attraction['ID']).name
-    
     attraction['flag'] == 1 ? name + '(ファストパス)' : name
+  end
+
+  def to_minute(time)
+    time.match(%r{(\d{2}):(\d{2})})
+    ($1.to_i*60 + $2.to_i).to_s
+  end
+
+  def parse_candidate(candidate)
+    { start: {
+        id:     candidate['start']['place'],
+        area_id: Attraction.find_by(algorithm_id: candidate['start']['place']).area_id,
+        name: '出発' || Attraction.find_by(algorithm_id: candidate['start']['place']).name,
+        time:  candidate['start']['time']
+    },
+      attractions: candidate['attraction'].map { |attraction|
+        {
+            id:    attraction['ID'],
+            name:   attraction_name(attraction),
+            area_id: Attraction.find_by(algorithm_id: attraction['ID']).area_id,
+            start:      attraction['start'],
+            move:       attraction['move'],
+            arrive:     attraction['arrive'],
+            wait:       attraction['wait'],
+            ride:       attraction['ride'],
+            duration:   attraction['duration'],
+            end:        attraction['end']
+        }
+      },
+      discription: candidate['discription']
+    }
   end
 end
